4d3186b93e62c6fc6ea39c7b50bc86d6135a6d7b
Rony de Lucca <rony.oliveira@altran-consultoria.com.br>
2022-01-29 23:16:31 -0300

    initial structure hardhat solidity

---
+++ contracts/Messenger.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Messenger {
+    constructor(){
+        console.log("Hey, here is the first message from this new messenger!");
+    }
+}
