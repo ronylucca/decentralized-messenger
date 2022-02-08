46f3abb1c46b163c43e06a93a5c6fcef76202cd3
Rony de Lucca <ronylucca@gmail.com>
2022-02-08 01:31:19 -0300

    Now we register text messages from interface

--- contracts/Messenger.sol
+++ contracts/Messenger.sol
@@ -7,17 +7,39 @@ import "hardhat/console.sol";
 contract Messenger {
 
     uint256 totalWaves;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave{
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+
+    Wave[] waves;
+
     constructor(){
         console.log("Hey, here is the first message from this new messenger!");
     }
 
-    function wave() public{
+    function wave(string memory _message) public{
+
         totalWaves +=1;
-        console.log("%s was waved!", msg.sender);
+        console.log("%s was waved with message %s", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
     function getTotalWaves() public view returns (uint256){
         console.log("We have a %d total waves!", totalWaves);
         return totalWaves;
+    }
+
+    function getAllWaves() public view returns (Wave[] memory){
+        return waves;
     }
 }
